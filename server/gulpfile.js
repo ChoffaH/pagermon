f3a682b234ea77ba076e6b6988b4cdc0abf9dfe6
Christopher Haglund <choffa@haglund.uk>
2025-02-08 14:03:44 +0100

    Use font awesome via npm

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -5,22 +5,28 @@ const sass = require("gulp-sass")(require("sass"));
 const shell = require("gulp-shell");
 const { parallel } = require("gulp");
 
+gulp.task("icons", function() {
+    return gulp.src("node_modules/@fortawesome/fontawesome-free/webfonts/*", {encoding: false})
+        .pipe(gulp.dest("./themes/default/public/assets/webfonts/"));
+});
+
 gulp.task("sass", function () {
   return gulp.src("./sass/style.scss")
     .pipe(sass({
-        loadPaths: ['node_modules'],
+        loadPaths: ["node_modules"],
         quietDeps: true,
         silenceDeprecations: ["import"],
     }).on("error", sass.logError))
-    .pipe(gulp.dest("./themes/default/public/stylesheets"));
+    .pipe(gulp.dest("./themes/default/public/assets/css"));
 });
 
 gulp.task("sass:watch", function () {
-    gulp.series("sass")();
     gulp.watch("./sass/*.scss", gulp.series("sass"));
 });
 
-gulp.task("server", shell.task("node app.js"));
+gulp.task("node", shell.task("node app.js"));
+
+gulp.task("server", gulp.series("sass", "icons", "node"));
 
 gulp.task("default", parallel("sass:watch", "server"));
 
