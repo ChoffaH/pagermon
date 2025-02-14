7f9c4f1e9e133bf67bf613fd43de656473f1e9cb
Christopher Haglund <choffa@haglund.uk>
2025-02-14 21:16:48 +0100

    Make using font awesome kit work again and default to prebuilt icons

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -5,29 +5,35 @@ const sass = require("gulp-sass")(require("sass"));
 const shell = require("gulp-shell");
 const { parallel } = require("gulp");
 
-gulp.task("icons", function() {
-    return gulp.src("node_modules/@fortawesome/fontawesome-free/webfonts/*", {encoding: false})
-        .pipe(gulp.dest("./themes/default/public/assets/webfonts/"));
+const sassOptions = {
+  loadPaths: ["node_modules"],
+  quietDeps: true,
+  silenceDeprecations: ["import"],
+  errLogToConsole: true
+};
+
+gulp.task("fa-fonts", function() {
+  return gulp.src("node_modules/@fortawesome/fontawesome-free/webfonts/*", {encoding: false})
+    .pipe(gulp.dest("./themes/default/public/assets/webfonts/"));
+});
+
+gulp.task("fa-sass", function () {
+  return gulp.src("./sass/fa.scss")
+    .pipe(sass(sassOptions))
+    .pipe(gulp.dest("./themes/default/public/assets/css"));
 });
 
 gulp.task("sass", function () {
   return gulp.src("./sass/style.scss")
-    .pipe(sass({
-        loadPaths: ["node_modules"],
-        quietDeps: true,
-        silenceDeprecations: ["import"],
-    }).on("error", sass.logError))
+    .pipe(sass(sassOptions))
     .pipe(gulp.dest("./themes/default/public/assets/css"));
 });
 
 gulp.task("sass:watch", function () {
-    gulp.watch("./sass/*.scss", gulp.series("sass"));
+  gulp.watch("./sass/*.scss", gulp.series("sass"));
 });
 
 gulp.task("node", shell.task("node app.js"));
-
-gulp.task("server", gulp.series("sass", "icons", "node"));
-
+gulp.task("server", gulp.series("fa-fonts", "fa-sass", "sass", "node"));
 gulp.task("default", parallel("sass:watch", "server"));
-
-gulp.task("build", gulp.series("sass", "icons"));
+gulp.task("build", gulp.series("fa-fonts", "fa-sass", "sass"));
